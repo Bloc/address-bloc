82e98262b2d5023f23024d3b9681dd454211bb66
Thomas Burns <tcburns24@gmail.com>
2017-09-18 11:35:21 -0700

    add circle.yml file

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -4,21 +4,27 @@ class MenuController
   attr_reader :address_book
 
   def initialize
-    @address_book = AddressBook.new
+    @address_book = AddressBook.first
   end
 
   def main_menu
-    puts "Main Menu - #{address_book.entries.count} entries"
+    puts "#{@address_book.name} Address Book Selected\n#{@address_book.entries.count} entries"
+    puts "0 - Switch AddressBook"
     puts "1 - View all entries"
     puts "2 - Create an entry"
     puts "3 - Search for an entry"
     puts "4 - Import entries from a CSV"
     puts "5 - Exit"
+    puts "6 - Test"
     print "Enter your selection: "
 
     selection = gets.to_i
 
     case selection
+      when 0
+        system "clear"
+        select_address_book_menu
+        main_menu
       when 1
         system "clear"
         view_all_entries
@@ -38,6 +44,9 @@ class MenuController
       when 5
         puts "Good-bye!"
         exit(0)
+      when 6
+        test_stuff
+        exit(0)
       else
         system "clear"
         puts "Sorry, that is not a valid input"
@@ -45,9 +54,91 @@ class MenuController
     end
   end
 
+  def test_stuff
+    originals = {}
+    news = {
+      1 => {
+        "name" => "Foo Uno",
+        "email" => "foo_uno@example.com"
+      },
+      2 => {
+        "name" => "Foo Dos",
+        "email" => "foo_dos@example.com"
+      }
+    }
+
+    puts "---- Original 1 and 2 ----"
+
+    Entry.find(news.keys).each do |entry|
+      puts entry
+      originals[entry.id] = {
+        "name" => entry.name,
+        "email" => entry.email
+      }
+    end
+
+    # Entry.update([1, 2], [{name => Foo Uno}, {name => Foo Dos}])
+    #
+    # Entry.update(1, {name => Foo Uno})
+    # Entry.update(2, {name => Foo Dos})
+    Entry.update(news.keys, news.values)
+
+    puts "---- New 1 and 2 ----"
+    puts Entry.where
+
+    Entry.update(originals.keys, originals.values)
+
+    puts "---- Back to Original 1 and 2 ----"
+    Entry.find_each do |item|
+      puts item
+    end
+
+    Entry.where(id: 3).update_all(
+      name: "Foo Tres",
+      email: "foo_tres@example.com"
+    )
+
+    puts "---- 3's New Name ----"
+    puts Entry.where(name: "Foo Tres") # Should return a collection with just Mr. Unique
+
+    puts "---- Everyone Besides New 3 ----"
+    puts Entry.not(name: "Foo Tres") # Should return a collection with everyone but Mr. Unique)
+
+    Entry.find(3).update_name("Foo Three")
+    Entry.find_one(3).update_email("foo_three@gmail.com")
+
+    puts "---- Back to Original 3 ----"
+    puts Entry.find_by_name("Foo Three")
+
+    puts "---- Again, Everyone Else (Should be unchanged) ----"
+    puts Entry.where(address_book_id: 1).not(name: "Foo Three")
+
+    puts "---- All Together Now ----"
+    puts Entry.find_all
+
+    puts "---- Delete Mitch Moreland ----"
+    puts Entry.destroy_all("name = 'Mitch Moreland'")
+  end
+
+  def select_address_book_menu
+    puts "Select an Address Book:"
+    AddressBook.all.each_with_index do |address_book, index|
+      puts "#{index} - #{address_book.name}"
+    end
+
+    index = gets.chomp.to_i
+
+    @address_book = AddressBook.find(index + 1)
+    system "clear"
+    return if @address_book
+    puts "Please select a valid index"
+    select_address_book_menu
+  end
+
   def view_all_entries
-    address_book.entries.each do |entry|
+    @address_book.entries.each do |entry|
       system "clear"
+      puts "Address Book: #{entry.address_book.name} Entry"
       puts entry.to_s
       entry_submenu(entry)
     end
@@ -75,7 +166,7 @@ class MenuController
   def search_entries
     print "Search by name: "
     name = gets.chomp
-    match = address_book.binary_search(name)
+    match = Entry.find_by(:name, name)
     system "clear"
     if match
       puts match.to_s
@@ -130,24 +221,33 @@ class MenuController
     end
   end
 
-  def delete_entry(entry)
-    address_book.entries.delete(entry)
-    puts "#{entry.name} has been deleted"
+  # Orinal `delete_entry` :
+  # def delete_entry(entry)
+  #   address_book.entries.delete(entry)
+  #   puts "#{entry.name} has been deleted"
+  # end
+
+  def delete_entry(entry_id)
+    self.destroy(entry_id)
   end
 
   def edit_entry(entry)
+    updates = {}
     print "Updated name: "
     name = gets.chomp
+    updates[:name] = name unless name.empty?
     print "Updated phone number: "
     phone_number = gets.chomp
+    updates[:phone_number] = phone_number unless phone_number.empty?
     print "Updated email: "
     email = gets.chomp
-    entry.name = name if !name.empty?
-    entry.phone_number = phone_number if !phone_number.empty?
-    entry.email = email if !email.empty?
+    updates[:email] = email unless email.empty?
+
+    entry.update_attributes(updates)
     system "clear"
     puts "Updated entry:"
     puts entry
+    puts Entry.find(entry.id)
   end
 
   def search_submenu(entry)
@@ -176,4 +276,3 @@ class MenuController
     end
   end
 end
-
